0a57bd1b8bb1128b16f3c19525e70d0035b8f0ab
Hugobsan <hugocorone10@gmail.com>
2024-11-02 05:45:35 -0300

    fix[AccountsPayable]: Corrigindo Seeders

--- contabilize-app/app/Models/AccountPayable.php
+++ contabilize-app/app/Models/AccountPayable.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use App\Enums\CategoryEnum;
 use App\Enums\StatusEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AccountPayable extends Model
 {
     use SoftDeletes;
+    use HasFactory;
     protected $table = "accounts_payable";
 
     protected $fillable = [
--- contabilize-app/database/seeders/AccountPayableSeeder.php
+++ contabilize-app/database/seeders/AccountPayableSeeder.php
@@ -12,6 +12,6 @@ class AccountPayableSeeder extends Seeder
      */
     public function run(): void
     {
-        AccountPayable::factory()->count(50)->create();
+        AccountPayable::factory(50)->create();
     }
 }
